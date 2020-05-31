5b39f583232744023d4db8b5f73a500bbc12257e
gezhizheng <gezhizheng@58.com>
2020-05-31 15:38:52 +0800

    事务处理

--- Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
+++ Chapter 2/declarative-transaction-demo/src/main/java/geektime/spring/data/declarativetransactiondemo/FooServiceImpl.java
@@ -10,6 +10,9 @@ public class FooServiceImpl implements FooService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private FooService fooService;
+
     @Override
     @Transactional
     public void insertRecord() {
@@ -25,6 +28,10 @@ public class FooServiceImpl implements FooService {
 
     @Override
     public void invokeInsertThenRollback() throws RollbackException {
+        // 通过this.insertThenRollback()调用的是原始类的方法，不是代理类的方法，无事务
         insertThenRollback();
+
+        // 这个方法能够保持在事务内部，因为从spring内部获取到的对象是代理类，所以存在事务。
+        fooService.insertThenRollback();
     }
 }
